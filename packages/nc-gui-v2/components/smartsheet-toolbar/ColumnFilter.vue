b853a325a0610dfff31a30d3176e9899f3426c06
Pranav C <pranavxc@gmail.com>
2022-07-29 23:19:58 +0530

    feat(gui-v2): filter auto-save disable/enable option
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/smartsheet-toolbar/ColumnFilter.vue
+++ packages/nc-gui-v2/components/smartsheet-toolbar/ColumnFilter.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { watchEffect } from '@vue/runtime-core'
 import { UITypes } from 'nocodb-sdk'
 import FieldListAutoCompleteDropdown from './FieldListAutoCompleteDropdown.vue'
-import Smartsheet from '~/components/tabs/Smartsheet.vue'
 import { useNuxtApp } from '#app'
 import { inject } from '#imports'
 import { comparisonOpList } from '~/utils/filterUtils'
@@ -11,7 +9,7 @@ import useViewFilters from '~/composables/useViewFilters'
 import MdiDeleteIcon from '~icons/mdi/close-box'
 import MdiAddIcon from '~icons/mdi/plus'
 
-const { nested = false, parentId } = defineProps<{ nested?: boolean; parentId?: string }>()
+const { nested = false, parentId, autoSave = true } = defineProps<{ nested?: boolean; parentId?: string; autoSave: boolean }>()
 
 const emit = defineEmits(['update:filters-length'])
 
@@ -21,15 +19,16 @@ const reloadDataHook = inject(ReloadViewDataHookInj)
 
 const { $e } = useNuxtApp()
 
-const { filters, deleteFilter, saveOrUpdate, loadFilters, addFilter, addFilterGroup } = useViewFilters(
+const { filters, deleteFilter, saveOrUpdate, loadFilters, addFilter, addFilterGroup, sync } = useViewFilters(
   activeView,
   parentId,
+  computed(() => autoSave),
   () => {
     reloadDataHook?.trigger()
   },
 )
 
-const filterUpdateCondition = (filter, i) => {
+const filterUpdateCondition = (filter, i:number) => {
   saveOrUpdate(filter, i)
   $e('a:filter:update', {
     logical: filter.logical_op,
@@ -79,7 +78,7 @@ watch(
   { immediate: true },
 )
 
-const nestedFilter = ref()
+const nestedFilters = ref()
 
 const logicalOps = [
   { value: 'and', text: 'AND' },
@@ -92,6 +91,22 @@ watch(
     emit('update:filters-length', length ?? 0)
   },
 )
+
+const applyChanges = async () => {
+  console.log('hello')
+  // sync()
+  // $e('a:filter:apply')
+
+    for (const nestedFilter of nestedFilters?.value || []) {
+      if (nestedFilter.parentId) {
+        await nestedFilter.applyChanges(true);
+      }
+    }
+}
+
+defineExpose({
+  applyChanges, parentId
+})
 </script>
 
 <template>
@@ -128,7 +143,7 @@ watch(
             <div class="col-span-5">
               <SmartsheetToolbarColumnFilter
                 v-if="filter.id || shared"
-                ref="nestedFilter"
+                ref="nestedFilters"
                 v-model="filter.children"
                 :parent-id="filter.id"
                 nested
